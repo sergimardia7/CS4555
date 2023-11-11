7253452ca9ef507d7064b7d3b7ca8f38536cdf08
sergimardia7 <Sergimardia@gmail.com>
2023-11-10 22:12:12 -0800

    Enemy AI Code
    Broken Code, please fix Enemy AI and Winston

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -8,6 +8,7 @@ public class EnemyAI : MonoBehaviour
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
+    public float health;
 
     //Patroling
     public Vector3 walkPoint;
@@ -16,6 +17,8 @@ public class EnemyAI : MonoBehaviour
 
     //Attacking
     public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
 
     //States
     public float sightRange, attackRange;
@@ -42,16 +45,39 @@ public class EnemyAI : MonoBehaviour
     {
         if (!walkPointSet) SearchWalkPoint();
         if (walkPointSet) agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
     }
 
     private void ChasePlayer()
     {
-
+        agent.SetDestination(Winston.position);
     }
 
     private void AttackPlayer()
     {
+        //Make sure Enemy doesnt move
+        agent.SetDestination(transform.position);
 
+        transform.LookAt(Winston);
+
+        if(!alreadyAttacked)
+        {
+           //Attack Code
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+           alreadyAttacked = true;
+           Invoke(nameof(ResetAttack), timeBetweenAttacks); 
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 
     private void SearchWalkPoint()
@@ -66,6 +92,26 @@ public class EnemyAI : MonoBehaviour
         {
             walkPointSet = true;
         }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy, .5f));
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 
 }
